a319aaa97f372d043cdaafbb7a0f9e5ce41cf92c
HelithaSri <helitha.pravin@gmail.com>
2022-03-07 23:27:53 +0530

    Search & Clear Search

--- myprojects/SPA/controller/ItemController.js
+++ myprojects/SPA/controller/ItemController.js
@@ -1,7 +1,21 @@
-addCustomer();
+/**
+ * @author : HelithaSri
+ * @email : helitha.pravin@gmail.com
+ * @create date : 2022-03-07  13:00
+ * @modify date : 2022-03-07  23:24
+ * @desc [ItemController]
+ */
+
+/* Functions Call Section - Start */
+
+addItem();  //Add New Item
+loadAllItems(); //Load All items
+clearSearch();  //Clear Search and Refresh table
+
+/* Functions Call Section - End */
 
 // Item Add Function - Start
-function addCustomer() {
+function addItem() {
     $("#btnAddItem").click(function () {
 
         let itemId = $("#itemCode").val();
@@ -15,18 +29,13 @@ function addCustomer() {
             __qty: itemQty,
             __price: itemPrice
         }
-        /* let btns =
-            "<button class='btn btn-warning' data-bs-target='#updateCustomer' data-bs-toggle='modal'><i class='bi bi-arrow-clockwise'></i></button> <button class='btn btn-danger'><i class='bi bi-trash'></i></button>";
-
-        customerObj.setCustomer(custId, custName, custAddress, custSalary, btns);
-        */
 
         itemDB.push(itemObj);
         console.log(itemDB);
         loadAllItems(); //load all Items
-        // Clear input Fields
-        $("#itemCode,#itemName,#itemQty,#itemPrice").val("");
-        //bindCustomerRow(); //bind the events to the table rows after the row was added
+        
+        $("#itemCode,#itemName,#itemQty,#itemPrice").val(""); // Clear input Fields
+        bindItemRow(); //bind the events to the table rows after the row was added
     });
 }
 // Item Add Function - End
@@ -55,3 +64,66 @@ function loadAllItems() {
     }
 }
 // Load All Items Function - End
+
+// Bind Events Item Row Function - Start
+function bindItemRow() {
+    $("#itemTblBody>tr").click(function () {
+        let itemId = $(this).children(":eq(0)").text();
+        let itemName = $(this).children(":eq(1)").text();
+        let itemQty = $(this).children(":eq(2)").text();
+        let itemPrice = $(this).children(":eq(3)").text();
+
+        $("#updateItemCode").val(itemId);
+        $("#updateItemName").val(itemName);
+        $("#updateItemQty").val(itemQty);
+        $("#updateItemPrice").val(itemPrice);
+    });
+}
+// Bind Events Item Row - End
+
+$("#btn-item-search").click(function () {
+    let btns =
+        "<button class='btn btn-warning' data-bs-target='#updateItem' data-bs-toggle='modal'><i class='bi bi-arrow-clockwise'></i></button> <button id='item-delete' class='btn btn-danger'><i class='bi bi-trash'></i></button>";
+
+    var searchId = $("#txt-item-search").val();
+    var response = searchItem(searchId);
+    if (response) {
+        $("#itemTblBody").empty();
+        let nRow =
+            "<tr><td>" +
+            response.__id +
+            "</td><td>" +
+            response.__name +
+            "</td><td>" +
+            response.__qty +
+            "</td><td>" +
+            response.__price +
+            "</td><td class='text-center'>" +
+            btns +
+            "</td></tr>";
+        $("#itemTblBody").append(nRow);
+        bindItemRow();
+    } else {
+        alert("No Such a Item");
+        clearSearch(); //Clear Search and Refresh table
+
+    }
+});
+
+function searchItem(id) {
+    for (let i = 0; i < itemDB.length; i++) {
+        if (itemDB[i].__id == id) {
+            return itemDB[i];
+        }
+    }
+}
+
+//clear search function - start
+function clearSearch() {
+    $("#clear-btn-item").click(function () {
+        console.log("sda");
+        loadAllItems(); //load all Items
+        $("#txt-item-search").val("");
+    });
+}
+//clear search function - End
