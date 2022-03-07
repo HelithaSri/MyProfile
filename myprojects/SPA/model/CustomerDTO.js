1a9e8b2de1b40a48defe90e28ad89a1978ec6eb1
HelithaSri <helitha.pravin@gmail.com>
2022-03-08 02:50:50 +0530

    Towards some changes

--- myprojects/SPA/model/CustomerDTO.js
+++ myprojects/SPA/model/CustomerDTO.js
@@ -1,55 +1,47 @@
-function CustomerObject(id,name,address,salary,btn) {
+function CustomerDTO(id,name,address,salary,btn) {
     var __cID =id;
     var __cName = name;
     var __cAddress = address;
     var __cSalary =salary;
     var __btn = btn;
 
-    this.setCustomer = function (id,name,address,salary,btn) {
-        this.__cID=id;
-        this.__cName=name;
-        this.__cAddress=address;
-        this.__cSalary=salary;
-        this.__btn=btn;
-    }
-
     this.getCustomerID = function () {
-        return this.__cID;
+        return __cID;
     }
 
     this.setCustomerID = function (id) {
-        this.__cID=id;
+        __cID=id;
     }
 
     this.getCustomerName = function () {
-        return this.__cName;
+        return __cName;
     }
 
     this.setCustomerName = function (name) {
-        this.__cName=name;
+        __cName=name;
     }
 
     this.getCustomerAddress = function () {
-        return this.__cAddress;
+        return __cAddress;
     }
 
     this.setCustomerAddress = function (address) {
-        this.__cAddress=address;
+        __cAddress=address;
     }
 
     this.getCustomerSalary = function () {
-        return this.__cSalary;
+        return __cSalary;
     }
 
     this.setCustomerSalary = function (salary) {
-        this.__cSalary=salary;
+        __cSalary=salary;
     }
 
     this.getCustomerbtn = function () {
-        return this.__btn;
+        return __btn;
     }
 
     this.setCustomerbtn = function (btn) {
-        this.__btn=btn;
+        __btn=btn;
     }
 }
