f2f379200cd03c415b79eada9869419429face21
HelithaSri <helitha.pravin@gmail.com>
2022-03-27 16:28:29 +0530

    Done

--- myprojects/SPA/controller/OrderDetailsController.js
+++ myprojects/SPA/controller/OrderDetailsController.js
@@ -1,19 +1,28 @@
-$("#btn-order-search").click(function(){
-    $("#orderDetailsTblBody").children().remove()
+$("#btn-order-search").click(function () {
+    $("#orderDetailsTblBody").children().remove();
     let result = $("#txt-order-search").val();
     searchOrderDetails(result);
-    
+
+});
+
+$("#clear-btn-order").click(function () {
+    $("#orderDetailsTblBody").children().remove();
+    $("#lblCusId").text("");
+    $("#lblDate").text("");
+    $("#lblSTotal").text("");
+    $("#lblOrderId").text("");
+    $("#txt-order-search").val("");
 });
 
 function searchOrderDetails(result) {
-    
+
     for (const od of orderDetailsDB) {
-        if (od.getOrderid()==result) {
+        if (od.getOrderid() == result) {
 
             $("#lblOrderId").text(od.getOrderid());
 
             for (const o of orderDB) {
-                if (o.getOrderId()==result) {
+                if (o.getOrderId() == result) {
                     $("#lblCusId").text(o.getCustomerId());
                     $("#lblDate").text(o.getDate());
                     $("#lblSTotal").text(o.getSubTotal());
@@ -23,10 +32,10 @@ function searchOrderDetails(result) {
             let row =
                 `<tr><td>${od.getItemCode()}</td><td>${od.getItemName()}</td><td>${od.getItemUnitPrice()}</td><td>${od.getItemQty()}<td>${od.getTotAmount()}</td></tr>`;
             $("#orderDetailsTblBody").append(row);
-           
-        }else{
+
+        } else {
             console.log(false);
         }
     }
-    
+
 }
