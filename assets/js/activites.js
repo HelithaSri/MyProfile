e9f02c7fa105099e50f62bfaa51ff858d3df40b1
HelithaSri <helitha.pravin@gmail.com>
2022-06-14 01:29:39 +0530

    Activity adds dynamically

--- assets/js/activites.js
+++ assets/js/activites.js
@@ -6,15 +6,15 @@
  * @description Fetch the JSON file and the featured item in the relevant activity is a video; Dynamically add the CODE BLOCK containing the relevant video.
  * @param {string} rsrcPath
  * @param {array} rsrcNames 
- * @param {string} filePath 
- * @param {string} filePath 
+ * @param {string} folderPath 
+ * @param {string} folderName 
  * @param {string} fileName 
  * @param {string} title 
  * @param {int} key 
  * @param {array} tags 
  * @param {boolean} setTitle 
  */
-function isVideo(rsrcPath, rsrcNames, filePath, fileName, title, currentIndex, tags, setTitle) {
+function isVideo(rsrcPath, rsrcNames, folderPath, folderName ,fileName, title, currentIndex, tags, setTitle) {
     let activeTitle;
     let sourceArray = [];
 
@@ -40,6 +40,19 @@ function isVideo(rsrcPath, rsrcNames, filePath, fileName, title, currentIndex, t
         sourceArray.push(source);
     }
 
+    let code_block = `<a href="${folderPath + folderName + "/" + fileName}">
+    <div class="portfolio_card">
+      
+      <video id="${currentIndex}" autoplay loop muted playsinline  class="portfolio_img videos">
+      Sorry, your browser doesn't support embedded videos.
+      </video>
+      
+      <span><i class="fas fa-tag"></i>${tags}</span>
+      <div class="port_title">${activeTitle}</div>
+    </div>
+  </a>`
 
+  $(".flex").append(code_block);    // code_block append to div
+  $(`.flex > a:nth-child(${currentIndex}) >.portfolio_card>video`).append(sourceArray);     //  append sources to video tag
 
 }
