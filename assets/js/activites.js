818f50bf573f4cab4031edfb5f80041b6c51ea29
HelithaSri <helitha.pravin@gmail.com>
2022-06-14 03:40:07 +0530

    Extract all activity from the JSON file and dynamically add the relevant CODE-BLOCK.

--- assets/js/activites.js
+++ assets/js/activites.js
@@ -1,6 +1,8 @@
 //featured item/img is video
 //rsrc --> Resource
 
+
+
 /**
  * @name isVideo
  * @description Fetch the JSON file and the featured item in the relevant activity is a video; Dynamically add the CODE BLOCK containing the relevant video.
@@ -14,13 +16,14 @@
  * @param {array} tags 
  * @param {boolean} setTitle 
  */
-function isVideo(rsrcPath, rsrcNames, folderPath, folderName ,fileName, title, currentIndex, tags, setTitle) {
+
+function isVideo(rsrcPath, rsrcNames, folderPath, folderName, fileName, title, currentIndex, tags, setTitle) {
     let activeTitle;
     let sourceArray = [];
-
+    console.log("currentI " + currentIndex);
     // Check title is full or half and assign it to ActiveTitle
     if (setTitle) {
-        activeTitle = folder + " - " + title;
+        activeTitle = folderName + " - " + title;
     } else {
         activeTitle = title;
     }
@@ -33,18 +36,19 @@ function isVideo(rsrcPath, rsrcNames, folderPath, folderName ,fileName, title, c
 
         if (fileExtention == "webm") {
             source = `<source src="${path}" type="video/webm"></source>`;
-        } else{
+        } else {
             source = `<source src="${path}" type="video/mp4"></source>`;
         }
-        
+
         sourceArray.push(source);
+        console.log(sourceArray);
     }
 
     let code_block = `<a href="${folderPath + folderName + "/" + fileName}">
     <div class="portfolio_card">
       
       <video id="${currentIndex}" autoplay loop muted playsinline  class="portfolio_img videos">
-      Sorry, your browser doesn't support embedded videos.
+      <p>Sorry, your browser doesn't support embedded videos.</p>
       </video>
       
       <span><i class="fas fa-tag"></i>${tags}</span>
@@ -52,7 +56,81 @@ function isVideo(rsrcPath, rsrcNames, folderPath, folderName ,fileName, title, c
     </div>
   </a>`
 
-  $(".flex").append(code_block);    // code_block append to div
-  $(`.flex > a:nth-child(${currentIndex}) >.portfolio_card>video`).append(sourceArray);     //  append sources to video tag
+    $(".flex").append(code_block);    // code_block append to div
+    $(`.flex > a:nth-child(${currentIndex}) >.portfolio_card>video`).append(sourceArray);     //  append sources to video tag
 
 }
+
+/**
+ * @name isImage
+ * @description Fetch the JSON file and the featured item in the relevant activity is a video; Dynamically add the CODE BLOCK containing the relevant video.
+ * @param {string} rsrcPath
+ * @param {array} rsrcNames 
+ * @param {string} folderPath 
+ * @param {string} folderName 
+ * @param {string} fileName 
+ * @param {string} title 
+ * @param {int} key 
+ * @param {array} tags 
+ * @param {boolean} setTitle 
+ */
+function isImage(rsrcPath, rsrcNames, folderPath, folderName, fileName, title, currentIndex, tags, setTitle) {
+    let activeTitle;
+    let elementID = "port_img" + "" + currentIndex;
+    let path = rsrcPath + rsrcNames;
+
+    // Check title is full or half and assign it to ActiveTitle
+    if (setTitle) {
+        activeTitle = folderName + " - " + title;
+    } else {
+        activeTitle = title;
+    }
+
+    let code_block = `<a href="${folderPath + folderName + "/" + fileName}">
+  <div class="portfolio_card">
+    <div id="${elementID}" class="portfolio_img"></div>
+    <span><i class="fas fa-tag"></i>${tags}</span>
+    <div class="port_title">${activeTitle}</div>
+  </div>
+</a>`
+
+    $(".flex").append(code_block);
+    injectCssRule(elementID, path);
+
+}
+
+function injectCssRule(elementID, path) {
+    document.styleSheets[0].insertRule(`#${elementID} {\
+          background: url(${path})\
+      }`, 0)
+}
+
+$.ajax({
+    url: "/projects.json",
+    method: "GET",
+    cache: false, //can disable cash 
+    success: function (resp) {
+        const activity = resp.activity;
+        const rsrc_path = activity.resources_path;
+        const folder_path = activity.folder_path;
+
+        for (const key in activity.data) {
+            let folder = activity.data[key].folder;
+            let file = activity.data[key].file;
+            let title = activity.data[key].title;
+            let setFullTitle = activity.data[key].setFullTitle;
+            let resources = activity.data[key].resources;
+            let setVideo = activity.data[key].setVideo;
+            let tags = activity.data[key].tags;
+
+
+            if (setVideo) {
+                isVideo(rsrc_path, resources, folder_path, folder, file, title, parseInt(key) + 1, tags, setFullTitle);
+
+            } else {
+                isImage(rsrc_path, resources, folder_path, folder, file, title, parseInt(key) + 1, tags, setFullTitle);
+            }
+        }
+
+    }
+});
